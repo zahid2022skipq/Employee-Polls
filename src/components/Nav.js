9d5571eef1ef80b4bf20dba6e2df695f32c12b9d
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-09-15 12:46:56 +0500

    Navbar done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,36 +1,42 @@
-import {Link} from "react-router-dom";
-import {connect} from "react-redux";
-import {handleLogout} from "../actions/authedUser";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { handleLogout } from "../actions/authedUser";
+import "./nav.css";
+const Nav = ({ dispatch, authedUserId }) => {
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(handleLogout());
+  };
 
-const Nav = ({dispatch, authedUserId}) => {
-
-    const logout = (e) => {
-        e.preventDefault();
-        dispatch(handleLogout());
-    };
-
-    return (
-        <nav className="flex justify-center space-x-4">
-            <Link to="/"
-                  className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</Link>
-            <Link to="/leaderboard"
-                  className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Leaderboard</Link>
-            <Link to="/new"
-                  className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">New
-                Poll</Link>
-            <span
-                className="font-medium px-3 py-2 text-slate-700"
-                data-testid="user-information">User: {authedUserId}</span>
-            <button onClick={logout}
-                    className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Logout
-            </button>
-        </nav>
-    );
+  return (
+    <nav className="nav-container">
+      <Link to="/" className="link active">
+        Home
+      </Link>
+      <Link to="/leaderboard" className="link">
+        Leaderboard
+      </Link>
+      <Link to="/new" className="link">
+        New Poll
+      </Link>
+      <span className="nav-user" data-testid="user-information">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/4333/4333609.png"
+          height={20}
+          width={20}
+          alt="user profile"
+        />
+        {authedUserId}
+      </span>
+      <button onClick={logout} className="btn-logout">
+        Logout
+      </button>
+    </nav>
+  );
 };
 
-const mapStateToProps = ({authedUser}) => ({
-    authedUserId: authedUser.id,
+const mapStateToProps = ({ authedUser }) => ({
+  authedUserId: authedUser.id,
 });
-
 
 export default connect(mapStateToProps)(Nav);
